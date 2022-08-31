3a4ef3f6bb34baacf5fcaba868b847ebd745e2c9
Markus <mark394e@stud.kea.dk>
2022-08-31 14:50:49 +0200

    functional text typewritten

---
+++ typewriter.js
@@ -0,0 +1,41 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", init);
+let i = 0;
+let hiddenText;
+let shownText;
+
+function init() {
+  hiddenText = document.querySelector("#hiddenText").textContent;
+  shownText = document.querySelector("#typewriter").textContent;
+
+  console.log("hiddenText length: " + hiddenText.length);
+  document.querySelector("#startButton").addEventListener("click", startTyping);
+}
+
+function startTyping() {
+  if (i < hiddenText.length) {
+    shownText = shownText + hiddenText[i];
+    console.log(shownText);
+
+    // if (hiddenText[i] === " ") {
+    //   // play space sound
+    // } else
+    i++;
+    console.log(i);
+    document.querySelector("#typewriter").textContent = shownText;
+
+    setTimeout(() => {
+      startTyping();
+    }, 150);
+  } else {
+    end();
+  }
+}
+
+function end() {
+  console.log("end");
+  document.querySelector("#startButton").addEventListener("click", startTyping);
+  i = 0;
+  shownText = "";
+}
